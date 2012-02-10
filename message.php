fed52ccc23fbc01b2097b84957c05bf7990f663d
marco <marco@5th.ch>
2012-02-10 01:28:32 +0100

    first commit

---
+++ message.php
@@ -0,0 +1,32 @@
+<?php ?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+  
+  <meta charset="utf-8">
+  <title></title>
+  
+  <link rel="stylesheet" type="text/css" href="">
+  <style type="text/css">
+  
+  </style>
+  
+  <script type="text/javascript">
+  
+  </head>
+  
+  <body>
+  
+    <div id="container">
+      <p>dear lazy clicker/visitor, would you please get your fingers on your keyboard and type in this URL:
+      http://5th.li/45_
+      
+      this short short link is private to real people. Robots won't be able to figure out... hmm at least til now</p>
+      
+    
+    </div>
+    
+  </body>
+</html>
---
+++ plugin.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: typer
+Plugin URI: http://5th.li/typer_
+Description: this plugin will ask the visitor to type in the URL by himself, if the requested URL is trailed by the character _
+Version: 0.1
+Author: @koma5
+Author URI: http://5th.ch/
+*/
+
+
+
+//yourls_add_action('pre_redirect', 'askToType');
+yourls_add_filter('site_url', 'askToType');
+
+function askToType($args) 
+{
+    $url = $args[0];
+    $code = $args[1];
+    
+    $lastChar = substr($url, 0, strlen($url)-1);
+    
+    if ($lastChar == '_')
+    {
+        die();
+    
+    
+    }
+    
+    
+    //echo $url . $code;
+    //die();
+}
+
+?>
